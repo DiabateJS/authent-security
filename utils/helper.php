283ef122c5830329a7e7b87bc63b81d15f6835c2
DiabateJS <diabatejs@yahoo.fr>
2021-12-13 19:46:02 +0100

    Mise en place de l'api

---
+++ utils/helper.php
@@ -0,0 +1,14 @@
+<?php
+
+class Helper {
+
+    public static function createResponseObject(){
+        return array (
+          "code" => Constants::$ERROR_CODE,
+          "message" => Constants::$EMPTY_STRING
+        );
+    }
+
+}
+
+?>
